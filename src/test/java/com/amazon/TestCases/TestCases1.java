e725b9d444c2d2df3ad4f8251268e73e1a92464c
rkatoch <rahul.katoch@techrbm.com>
2025-02-04 13:10:11 +0530

    ss

---
+++ src/test/java/com/amazon/TestCases/TestCases1.java
@@ -0,0 +1,13 @@
+package com.amazon.TestCases;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class TestCases1 {
+   WebDriver driver;
+    @Test
+    public void testCase(){
+        driver=new ChromeDriver();
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/4.28.1");
+    }
+
+}
