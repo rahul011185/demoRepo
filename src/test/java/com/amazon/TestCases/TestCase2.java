074e4de235b3bf6234a599140a53f049fb0ebac2
rkatoch <rahul.katoch@techrbm.com>
2025-02-04 15:04:23 +0530

    line are removed

--- src/test/java/com/amazon/TestCases/TestCase2.java
+++ src/test/java/com/amazon/TestCases/TestCase2.java
@@ -1,12 +1,7 @@
 package com.amazon.TestCases;
-
 public class TestCase2 {
-
-
     public void TestCase2(){
 
-
         System.out.println("test case 2");
-        System.out.println("test case 3");
     }
 }
